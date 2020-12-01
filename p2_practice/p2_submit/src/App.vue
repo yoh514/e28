3c607d9d0713750e75692cfe85b8684a330fde37
yoh514 <52469446+yoh514@users.noreply.github.com>
2020-12-02 08:11:44 +0900

    Create App.vue

---
+++ p2_practice/p2_submit/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+<script>
+  import Header from '@/components/Header.vue';
+  import Footer from '@/components/Footer.vue';
+
+export default {
+  mounted() {
+    $(".selection-1").select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $('#dropDownSelect1')
+      });
+
+    $.initialize();
+  },
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
